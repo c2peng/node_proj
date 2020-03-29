2fd4428ca7c3aac3015e9ee4fcd8fa9e4a912eef
Cheng Peng <c2peng@ucla.edu>
2020-03-28 22:36:48 -0700

    Product redefinition

--- models/product.js
+++ models/product.js
@@ -1,31 +1,27 @@
-const cart = require('./cart');
-const db = require('../utils/database');
+const Sequelize = require('sequelize');
 
-module.exports = class product {
-    constructor(id, title, imageUrl, description, price) {
-        this.id = id;
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
+const sequelize = require('../utils/database.js');
+
+const Product = sequelize.define('Product', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoincrement: true,
+        allownull: false,
+        primaryKey: true
+    },
+    title: Sequelize.STRING,
+    price: {
+        type: Sequelize.DOUBLE,
+        allownull: false,
+    },
+    image: {
+        type: Sequelize.STRING,
+        allownull: false,
+    },
+    description: {
+        type: Sequelize.STRING,
+        allownull: false,
     }
+});
 
-    save() {
-        return db.execute('INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',
-            [this.title, this.price, this.description, this.imageUrl]);
-    }
-
-    static deleteById(id) {
-
-    }
-
-
-
-    static fetchAll() {
-        return db.execute('SELECT * FROM products');
-    }
-
-    static findById(id) {
-        return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-    }
-};
+module.exports = Product;
